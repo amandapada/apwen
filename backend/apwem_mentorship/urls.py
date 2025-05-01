16fa1b591e07845aea1fc2f3389d10fae5be9b7e
Khadijat Khalid <khadijakhalid128@gmail.com>
2025-05-01 22:16:18 +0100

    update links

--- backend/apwem_mentorship/urls.py
+++ backend/apwem_mentorship/urls.py
@@ -14,6 +14,15 @@ urlpatterns = [
     path("api/chat/", include("chat.urls")),  # Include chat-related URLs
     path("api/forum/", include("forum.urls")),  # Include forum-related URLs
     path('', TemplateView.as_view(template_name="index.html"), name="home"),
+    path('about-us', TemplateView.as_view(template_name="about.html"), name="about"),
+    path('events', TemplateView.as_view(template_name="events.html"), name="events"),
+    path('sign-in', TemplateView.as_view(template_name="sign_in.html"), name="sign_in"),
+    path('sign-up', TemplateView.as_view(template_name="sign_up.html"), name="sign_up"),
+    path('resources', TemplateView.as_view(template_name="resource.html"), name="sign_up"),
+    path('mentorship', TemplateView.as_view(template_name="mentorship.html"), name="sign_up"),
+
+
+    
 ]
 
 # Serve media files in development
