23515407e4b19da1fd288641ce6bc113957eac9b
Khadijat Khalid <khadijakhalid128@gmail.com>
2025-05-14 07:40:42 +0100

    add mentors

--- backend/mentors/views.py
+++ backend/mentors/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Mentor, Mentee
+from django.contrib.auth.decorators import login_required
 
 def add_mentor_view(request):
     if request.method == 'POST':
@@ -32,24 +33,26 @@ def add_mentor_view(request):
 
     return render(request, 'mentors.html')
 
+
 def add_mentee_view(request):
     if request.method == 'POST':
         first_name = request.POST.get('first-name')
         last_name = request.POST.get('last-name')
         other_name = request.POST.get('other-name')
         department = request.POST.get('departments')
-        mentor_department = request.POST.get('mentor-department')  # Updated name
+        mentor_department = request.POST.get('mentor-department')
         level = request.POST.get('mentor-level')
         phone_number = request.POST.get('phone-number')
         email = request.POST.get('email')
 
         if Mentee.objects.filter(email=email).exists():
             messages.error(request, 'Mentee with this email already exists.')
-            return redirect('add-mentee-view')
+            return redirect('mentees')
 
         if not all([first_name, last_name, department, mentor_department, level, phone_number, email]):
+           
             messages.error(request, "Please fill in all required fields.")
-            return redirect('add-mentee-view')
+            return redirect('mentees')
 
         mentee = Mentee.objects.create(
             first_name=first_name,
@@ -62,12 +65,14 @@ def add_mentee_view(request):
             email=email,
         )
         messages.success(request, 'Mentee registered successfully.')
-        return redirect('add-mentee-view')
-
-    return render(request, 'mentees.html')
+        return redirect('mentees')
 
 
+    return redirect('mentees')
 
+
+
+# @login_required
 def list_mentors_view(request):
     mentors = Mentor.objects.all()
 
@@ -76,5 +81,4 @@ def list_mentors_view(request):
 def list_mentee_view(request):
     mentees = Mentee.objects.all()
 
-    print(mentees)
     return render(request, 'mentees.html', {'mentees': mentees})
